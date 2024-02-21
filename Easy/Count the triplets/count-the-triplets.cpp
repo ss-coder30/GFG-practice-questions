c71770e8bdd9549c34fae7d810fd82415f0991c7
SHAURYA SRIVASTAVA <30shaurya@gmail.com>
2024-02-21 12:44:48 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Count the triplets/count-the-triplets.cpp
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	
+	int countTriplet(int arr[], int n)
+	{
+	    // Your code goes here
+	    sort(arr, arr+n);
+	    int count = 0;
+	    
+	    for(int i = n-1; i>=0; i--){
+	        int j = 0;
+	        int k = i-1;
+	        
+	        while(j < k){
+	            if(arr[i] == arr[j] + arr[k]){
+	                count++;
+	                j++;
+	                k--;
+	            }
+	            else if(arr[i] > arr[j] + arr[k]){
+	                j++;
+	            }
+	            else{
+	                k--;
+	            }
+	        }
+	    }
+	    
+	    return count;
+	}
+};
+
+//{ Driver Code Starts.
+
+
+int main() 
+{
+   	
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        int arr[n];
+        for(int i = 0; i < n; i++)
+        	cin >> arr[i];
+
+        Solution ob;
+        cout << ob.countTriplet(arr, n) << "\n";
+
+    }
+
+    return 0;
+}
+// } Driver Code Ends
